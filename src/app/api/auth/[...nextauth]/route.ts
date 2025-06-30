77873f096dd2203c0ab2c0d6eb508248c902f33c
Sreejith Sreejayan <51023498+the-Sreejith@users.noreply.github.com>
2025-06-30 10:33:03 +0530

    Fix NextAuth cookies scope error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-const handler = NextAuth(authOptions);
+export async function GET(req: Request) {
+  const handler = NextAuth(authOptions);
+  return handler(req);
+}
 
-export { handler as GET, handler as POST };
+export async function POST(req: Request) {
+  const handler = NextAuth(authOptions);
+  return handler(req);
+}
